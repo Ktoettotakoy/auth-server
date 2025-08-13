aed92302ca0a2376650e2cd53f22e0ddb664d47c
Ktoettotakoy <98839231+Ktoettotakoy@users.noreply.github.com>
2025-08-13 14:10:57 +0100

    added error proper minimal handling

---
+++ src/error.rs
@@ -0,0 +1,60 @@
+use serde::Serialize;
+use std::convert::Infallible;
+use thiserror::Error;
+use warp::{http::StatusCode, Rejection, Reply};
+
+#[derive(Error, Debug)]
+pub enum Error {
+
+    #[error("Invalid credentials")]
+    InvalidCredentialsError,
+
+    #[error("Missing authorization header")]
+    MissingAuthorizationHeader,
+    #[error("Invalid authorization header")]
+    InvalidAuthorizationHeader,
+
+    #[error("JWT token not valid")]
+    JWTTokenError,
+    #[error("JWT token creation error")]
+    JWTTokenCreationError,
+
+    #[error("No permission error")]
+    NoPermissionError,
+}
+
+#[derive(Serialize, Debug)]
+struct ErrorResponse {
+    message: String,
+    status: String,
+}
+
+impl warp::reject::Reject for Error {}
+
+pub async fn handle_rejection(err: Rejection) -> std::result::Result<impl Reply, Infallible> {
+    let (code,message) = if err.is_not_found() {
+        (StatusCode::NOT_FOUND, "Not Found".to_string())
+    } else if let Some(e) = err.find::<Error>() {
+        match e {
+            Error::InvalidCredentialsError => (StatusCode::UNAUTHORIZED, e.to_string()),
+            Error::MissingAuthorizationHeader => (StatusCode::UNAUTHORIZED, e.to_string()),
+            Error::InvalidAuthorizationHeader => (StatusCode::UNAUTHORIZED, e.to_string()),
+            Error::JWTTokenError => (StatusCode::UNAUTHORIZED, e.to_string()),
+            Error::JWTTokenCreationError => (StatusCode::INTERNAL_SERVER_ERROR, "Internal Server Error".to_string()),
+            Error::NoPermissionError => (StatusCode::UNAUTHORIZED, e.to_string()),
+            // _ => (StatusCode::BAD_REQUEST, e.to_string()),
+        }
+    } else if err.find::<warp::reject::MethodNotAllowed>().is_some() {
+        (StatusCode::METHOD_NOT_ALLOWED, "Method Not Allowed".to_string())
+    } else {
+        eprintln!("Unhandled error: {:?}", err);
+        (StatusCode::INTERNAL_SERVER_ERROR, "Internal Server Error".to_string())
+    };
+
+    let json = warp::reply::json(&ErrorResponse {
+        status: code.to_string(),
+        message,
+    });
+
+    Ok(warp::reply::with_status(json, code))
+}
